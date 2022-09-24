71aa6f19c0ec016fe93d78322f547281a4b4b7c8
we1u <we1u@email.com>
2022-09-24 11:45:38 +0200

    Fixed JavaDoc and variable names.

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -24,28 +24,28 @@ public class Aufgabe1 {
 	}
 
 	/**
-	 * Dummy Constructor
+	 * Dummy constructor
 	 */
 	public Aufgabe1() {
 	}
 
 	/**
-	 * Forces the user to select text- and pattern-file and applies the pattern on the text Result in console!
+	 * Forces the user to select text and pattern file and applies the pattern to the text result in console.
 	 */
 	public void performCheck() {
 
-		String filepath = getTextPath("Please choose your text!");
-		String text = getTextFromFile(filepath);
+		String filePath = getTextPath("Please choose your text!");
+		String text = getTextFromFile(filePath);
 
-		filepath = getTextPath("Please choose your pattern!");
-		String pattern = getTextFromFile(filepath);
+		filePath = getTextPath("Please choose your pattern!");
+		String pattern = getTextFromFile(filePath);
 		pattern = toRegEx(pattern);
 
 		if (text.isEmpty() || text.isBlank()) {
-			System.err.println("Invalid Text File");
+			System.err.println("Invalid Text File.");
 		}
 		if (pattern.isEmpty() || pattern.isBlank()) {
-			System.err.println("Invalid Pattern File");
+			System.err.println("Invalid Pattern File.");
 		}
 
 		Matcher m = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE).matcher(text);
@@ -55,31 +55,31 @@ public class Aufgabe1 {
 	}
 
 	/**
-	 * Opens a {@link JFileChooser} where the user can select a file and returns the path of this file
+	 * Opens a {@link JFileChooser} where the user can select a file and returns the path of the chosen file.
 	 *
-	 * @param dialoguetitle The title of the {@link JFileChooser}
-	 * @return The {@link Path} of the selected file as a String
+	 * @param dialogueTitle The title of the {@link JFileChooser}
+	 * @return The {@link Path} of the selected file as a string
 	 */
-	private String getTextPath(String dialoguetitle) {
+	private String getTextPath(String dialogueTitle) {
 		JFileChooser chooser = new JFileChooser();
 		chooser.setApproveButtonText("Load");
-		chooser.setDialogTitle(dialoguetitle);
+		chooser.setDialogTitle(dialogueTitle);
 		chooser.setMultiSelectionEnabled(false);
 		chooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
 
-		int rueckgabeWert = chooser.showOpenDialog(null);
+		int returnOption = chooser.showOpenDialog(null);
 
-		if (rueckgabeWert == JFileChooser.APPROVE_OPTION) {
+		if (returnOption == JFileChooser.APPROVE_OPTION) {
 			return chooser.getSelectedFile().getPath();
 		}
 		return "";
 	}
 
 	/**
-	 * Reads the given file and returns the file's content as a String
+	 * Reads the given file and returns the file's content as a string.
 	 *
-	 * @param path The Path of the File which should get loaded.
-	 * @return The content of the file as a String
+	 * @param path The path of the file to be loaded
+	 * @return The content of the file as a string
 	 */
 	private String getTextFromFile(String path) {
 
@@ -95,14 +95,14 @@ public class Aufgabe1 {
 	}
 
 	/**
-	 * Converts the given pattern-string into a regular expression
+	 * Converts the given pattern string into a regular expression.
 	 *
-	 * @param patternstring The pattern which should be converted into a RegEx
-	 * @return The created RegEx as a String
+	 * @param patternString The pattern which should be converted into a RegEx
+	 * @return The created RegEx as a string
 	 */
-	private String toRegEx(String patternstring) {
+	private String toRegEx(String patternString) {
 
-		return patternstring.replaceAll("_", "\\\\p{Alnum}+");
+		return patternString.replaceAll("_", "\\\\p{Alnum}+");
 	}
 
 }
